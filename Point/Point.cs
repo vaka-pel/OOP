056e8e1d897fa6f13ed36b28e0e5dfeafae1cd2c
Pelivan <Pelivan@DESKTOP-29KL5UV>
2025-10-27 21:43:19 +0300

    Add project files.

---
+++ Point/Point.cs
@@ -0,0 +1,93 @@
+﻿
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Point
+{
+	//internal class Point
+	//{
+
+	//	double x;
+	//	double y;
+	//	public double X
+	//	{
+	//		get
+	//		{
+	//			return x;
+	//		}
+	//		set
+	//		{
+	//			if (value > 100) value = 100;
+	//			x = value;
+	//		}
+	//	}
+	//	public double Y
+	//	{
+	//		get
+	//		{
+	//			return y;
+	//		}
+	//		set
+	//		{
+	//			if (value > 100) value = 100;
+	//			y = value;
+	//		}
+	//	} 
+	class Point
+	{
+		public double X { get; set; }
+		public double Y { get; set; }
+		//public double GetX()
+		//{
+		//	return x;
+		//}
+		//public double GetY()
+		//{
+		//	return y;
+		//}
+		//public void SetX(double x)
+		//{
+		//	this.x = x;
+		//}
+		//public void SetY(double y)
+		//{
+		//	this.y = y;
+		//}
+		public Point(double x = 0, double y = 0)
+	    {
+		X = x;
+		Y = y;
+		Console.WriteLine($"Constructor:\t{this.GetHashCode()}");
+	    }
+		public Point(Point other)
+		{
+			this.X = other.X;
+			this.Y = other.Y;
+			Console.WriteLine($"CopyConstructor:{this.GetHashCode()}");
+		}
+	    ~Point()
+	    {
+		Console.WriteLine($"Destructor:\t{this.GetHashCode()}");
+	    }
+	// Operators:
+	public static Point operator +(Point left, Point right)
+		{
+			return new Point
+				(
+				left.X+right.X,
+				left.Y+right.Y
+				);
+		}
+		public void Info()
+		{
+			Console.WriteLine($"X = {X}, Y = {Y}");
+		}
+	  
+	
+	   
+    }
+}
---
+++ Point/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Point
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Point A = new Point();
+			A.X = 2;
+			A.Y = 3;
+			//A.SetX(2);
+			//A.SetY(3);
+			Console.WriteLine($"X = {A.X}, Y = {A.Y}");
+			A.Info();
+
+			Point B = new Point(A);
+			B.X *= 10;
+			B.Info();
+			A.Info();
+
+			Point C = A + B;
+			C.Info();
+		}
+	}
+}
