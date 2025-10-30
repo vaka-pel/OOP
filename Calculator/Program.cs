5faa879c325eff393bf2db39a4790f57e0ed00bc
Pelivan <Pelivan@DESKTOP-29KL5UV>
2025-10-30 21:27:10 +0300

    Shift DONE

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -11,18 +11,24 @@ namespace Calculator
 {
 	internal class Program
 	{
+		static string expression = "";
+		static readonly char[] operators = new char[] { '+', '-', '*', '/' };
+		static string[] operands;
+		static double[] values;
+		static readonly char[] digits = "0123456789.".ToCharArray();
+		static string[] operations;
 		static void Main(string[] args)
 		{
 			Console.Write("Введите арифметическое выражение: ");
-			string expression = "22+33-44/2+8*3";
+			//string expression = "22+33-44/2+8*3";
+			expression = "22+33-44/2+8*3+1";
 			//string expression = Console.ReadLine();
 			expression = expression.Replace(".", ",");
 			expression = expression.Replace(" ", "");
 			Console.WriteLine(expression);
 
-			char[] operators = new char[] { '+', '-', '*', '/' };
-			string[] operands = expression.Split(operators);
-			double[] values = new double[operands.Length];
+			operands = expression.Split(operators);
+			values = new double[operands.Length];
 			for (int i = 0; i < operands.Length; i++)
 			{
 				values[i] = Convert.ToDouble(operands[i]);
@@ -30,20 +36,46 @@ namespace Calculator
 			}
 			Console.WriteLine();
 
-			char[] digits = "0123456789.".ToCharArray();
 			/*for (int i = 0; i < digits.Length; i++)
 			{
 				Console.Write($"{digits[i]}\t");
 			}
 			Console.WriteLine();*/
 
-			string[] operations = expression.Split(digits);
-			operations = operations.Where(o  => o != "").ToArray(); //LINQ
+			operations = expression.Split(digits);
+			operations = operations.Where(operation => operation != "").ToArray();  //LINQ
 			for (int i = 0; i < operations.Length; i++)
 			{
-				Console.Write($"{ operations[i]}\t");
+				Console.Write($"{operations[i]}\t");
 			}
 			Console.WriteLine();
+
+			while (operations[0] != "")
+			{
+				//int i = 0;
+				for (int i = 0; i < operations.Length; i++)
+				{
+					if (operations[i] == "*" || operations[i] == "/")
+					{
+						if (operations[i] == "*") values[i] *= values[i + 1];
+						if (operations[i] == "/") values[i] /= values[i + 1];
+						Shift(i);
+						if (operations[i] == "*" || operations[i] == "/") i--;
+					}
+				}
+				for (int i = 0; i < operations.Length; i++)
+				{
+					if (operations[i] == "+" || operations[i] == "-")
+					{
+						if (operations[i] == "+") values[i] += values[i + 1];
+						if (operations[i] == "-") values[i] -= values[i + 1];
+						Shift(i);
+						if (operations[i] == "+" || operations[i] == "-") i--;
+					}
+				}
+			}
+			Console.WriteLine(values[0]);
+
 
 #if CALC_IF
 			if (expression.Contains("+"))
@@ -66,8 +98,13 @@ namespace Calculator
 				case '/': Console.WriteLine($"{values[0]} / {values[1]} = {values[0] / values[1]}"); break;
 			} 
 #endif
-
-
+		}
+		static void Shift(int index)
+		{
+			for (int i = index; i < operations.Length - 1; i++) operations[i] = operations[i + 1];
+			for (int i = index + 1; i < values.Length - 1; i++) values[i] = values[i + 1];
+			operations[operations.Length - 1] = "";
+			values[values.Length - 1] = 0;
 
 		}
 	}
